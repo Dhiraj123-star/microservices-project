cc37c1fb2b4eaf1929c4750f4f76616746fd74f8
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-12-18 11:36:16 +0530

    feat(gateway): implement fastapi logic to push tasks to redis

---
+++ gateway-service/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import redis
+import os
+
+app = FastAPI()
+
+REDIS_HOST = os.getenv("REDIS_HOST","redis-master")
+REDIS_PORT=6379
+
+db=redis.Redis(host=REDIS_HOST,port=REDIS_PORT,decode_responses=True)
+
+@app.get("/")
+def read_root():
+    return {"message":"Gateway Service is running"}
+
+@app.post("/submit-task")
+def submit_task(task_name:str):
+    db.rpush("tasks",task_name)
+    return {"status":"Task submitted","task":task_name}
