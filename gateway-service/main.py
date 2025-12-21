88cad2cb46305bc66f0127ea89227c62d63ee371
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-12-21 13:04:15 +0530

    refractor(worker): use REDIS_PORT environment variable

--- gateway-service/main.py
+++ gateway-service/main.py
@@ -5,7 +5,7 @@ import os
 app = FastAPI()
 
 REDIS_HOST = os.getenv("REDIS_HOST","redis-master")
-REDIS_PORT=6379
+REDIS_PORT=int(os.getenv("REDIS_PORT",6379))
 
 db=redis.Redis(host=REDIS_HOST,port=REDIS_PORT,decode_responses=True)
 
